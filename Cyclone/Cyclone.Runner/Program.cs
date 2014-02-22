2b6826c9b5848ca2e03a15196da73d52251e17f6
Svein Erik <sundstei@gmail.com>
2014-02-22 11:08:17 +0100

    Made test program run interations of 10 000 messages with medium size.

--- Cyclone/Cyclone.Runner/Program.cs
+++ Cyclone/Cyclone.Runner/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace Cyclone.Runner
 {
+    /// <summary>
+    /// Test program to run actual integration test with the RabbitMQ client.
+    /// </summary>
     internal class Program
     {
         private static void Main(string[] args)
@@ -13,15 +16,16 @@ namespace Cyclone.Runner
             const string routingKey = "#";
             int counter = 0;
 
-            IDictionary<string, string> data = Enumerable.Range(100, 200).ToDictionary(x => x.ToString(), y => y.ToString());
+            IDictionary<string, string> data = Enumerable.Range(100, 110).ToDictionary(x => x.ToString(), y => y.ToString());
 
             var client = new SharedMessageClient<IDictionary<string, string>>(exchangeName, queueName, routingKey);
-            Enumerable.Range(1, 100000).ToList().ForEach(x => client.Publish(data));
+            Enumerable.Range(1, 10000).ToList().ForEach(x => client.Publish(data));
 
             client.AppendSubscription(x =>
                                       {
                                           counter++;
-                                          Console.WriteLine("Got a dictionary. " + counter);
+                                          if(counter % 1000 == 0)
+                                            Console.WriteLine("Received Data : " + counter.ToString("N0"));
                                       });
             Console.ReadKey();
         }
